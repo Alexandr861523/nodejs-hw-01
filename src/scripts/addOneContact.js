65662b087a18ef02649ade8c5061f8d463a94e6b
Alexandr861523 <alekgur128@gmail.com>
2024-09-08 16:11:51 +0300

    node-hw-01

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,15 @@
-export const addOneContact = async () => {};
+import createFakeContact from '../utils/createFakeContact.js';
+
+import getAllContacts from './getAllContacts.js';
+
+import updateContacts from './updateContacts.js';
+
+const addOneContact = async () => {
+	const contactList = await getAllContacts();
+	const newContactList = createFakeContact();
+	contactList.push(newContactList);
+	
+	await updateContacts(contactList);
+};
 
 addOneContact();
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import *  as fs from 'node:fs/promises';
 
+import { PATH_DB } from '../constants/contacts.js';
+
+const getAllContacts = async () => {
+	const data = await fs.readFile(PATH_DB, 'utf-8');
+	return JSON.parse(data);
+
+};
+
+export default getAllContacts;
 console.log(await getAllContacts());
